4eb854884a3aeffc480c12ac87488b78f2eca589
Lingcao Huang <huanglingcao@gmail.com>
2020-05-21 14:02:14 +0800

    add a new version before further modification

--- TomoWarp2_v2.1.320/DIC_setup_lessMemory.py
+++ TomoWarp2_v2.1.320/DIC_setup_lessMemory.py
@@ -71,10 +71,16 @@ def display_time(seconds, granularity=2):
             result.append("{} {}".format( int( value ), name))
     return ', '.join(result[:granularity])
 
+import os
+import psutil
+from psutil import virtual_memory
+process = psutil.Process(os.getpid())
 
+import gc
 # ===========================
 # === Program Starts Here ===
 # ===========================
+# @profile
 def DIC_setup( kinematics, data, q_data_requests, workerQueues ):
 
     # Wake up the data_delivery_worker with a new "data" array.
@@ -83,23 +89,33 @@ def DIC_setup( kinematics, data, q_data_requests, workerQueues ):
     # --- Define the extent of data we need to ask for ---
     # ----- i.e., generate extents matrix ----------------
     # 2014-10-04 EA and ET: updating extents matrix to a 4D array, with { node number }, { im_number 0,1 }, { top, bottom }, { z, y, x }
-    extents = numpy.zeros( ( kinematics.shape[0], 2, 2, 3 ), dtype=numpy.int )
+    extents = numpy.zeros( ( kinematics.shape[0], 2, 2, 3 ), dtype=numpy.int16 )    # change int32 to int16, to save memory (the extent would not be too large)
+
+    print ("finished creating extents array for all nodes, memory usage in bytes, GB, process id" ,process.memory_info().rss,process.memory_info().rss/(1024*1024*1024.0), process.pid)
 
     #                     position            correlation_window            top extent of search window     prior displacement
     # --- Handling im1_lo
     extents[:,0,0] = kinematics[:,1:4] - data.correlation_window
+    gc.collect()
     # --- Handling im1_hi
     extents[:,0,1] = kinematics[:,1:4] + data.correlation_window
+    gc.collect()
     # --- Handling im2_lo
     extents[:,1,0] = kinematics[:,1:4] - data.correlation_window + numpy.array( data.search_window )[:,0] +  kinematics[:,4:7]
+    gc.collect()
     # --- Handling im2_hi
     extents[:,1,1] = kinematics[:,1:4] + data.correlation_window + numpy.array( data.search_window )[:,1] +  kinematics[:,4:7]
+    gc.collect()
     # ----------------------------------------------------
 
     # Extents can not exceed the image_slices_extents
     extents[:,:,0,0] = numpy.maximum( extents[:,:,0,0], numpy.ones_like(extents[:,:,0,0]) * data.image_slices_extent[:,0] )
+    gc.collect()
     extents[:,:,1,0] = numpy.minimum( extents[:,:,1,0], numpy.ones_like(extents[:,:,1,0]) * data.image_slices_extent[:,1] )
+    gc.collect()
 
+    print ("finished extents calculation, memory usage in bytes, GB, process id", process.memory_info().rss,
+    process.memory_info().rss / (1024 * 1024 * 1024.0), process.pid)
 
     # --- Set Up Queues ---
     # This queue will contain the nodes for the DIC workers to process
@@ -108,12 +124,17 @@ def DIC_setup( kinematics, data, q_data_requests, workerQueues ):
     # This queue will contain the results for each node
     q_results       = multiprocessing.Queue( )
 
+    print ("finished Setting Up q_nodes and q_results, memory usage in bytes, GB, process id", process.memory_info().rss,
+    process.memory_info().rss / (1024 * 1024 * 1024.0), process.pid)
 
     # --- Launch DIC worker nodes  ---
     for workerNumber in range( data.nWorkers ):
         p = multiprocessing.Process( target=DIC_worker, args=( workerNumber, q_nodes, q_results, q_data_requests, workerQueues[ workerNumber ], data ) )
         p.start()
     # ----------------------------------
+
+    print ("finished Launching DIC worker, memory usage in bytes, GB, process id", process.memory_info().rss,
+    process.memory_info().rss / (1024 * 1024 * 1024.0), process.pid)
 
     # Calculate the highest slice we need
     currentTopSlice_image1 = min(extents[:,0,0,0])
@@ -153,12 +174,31 @@ def DIC_setup( kinematics, data, q_data_requests, workerQueues ):
     prevNodesProcessed = 0
     # ----------------------------------------------------------
 
+    max_nodes_in_quene = 5000
+    mem = virtual_memory()
+    # mem.total  # total physical memory
+    # mem.available # total available memory
+    # each node, need to copy subvolume of image
+    print(data.correlation_window)
+    print(data.search_window)
+    corr_size = [ win*2 +1  for win in data.correlation_window ]
+    print('correlation size',corr_size)
+    subVolume1 = corr_size[0]*corr_size[1]*corr_size[2]*4  # bytes
+    search_size = [ win*2 + 1 + abs(ed[0]) + abs(ed[1]) for win, ed in zip(data.correlation_window,data.search_window) ]
+    subVolume2 = search_size[0]*search_size[1]*search_size[2]*4 # bytes
+    print("search_size",search_size)
+    bytes_for_received_result = kinematics.shape[0]*9*4 # bytes
+    print("available memory (GB), reserved for results: ",mem.available/(1024.0*1024*1024.0),bytes_for_received_result/(1024.0*1024*1024.0))
+    max_nodes_in_quene = (mem.available - bytes_for_received_result)/(subVolume1+subVolume2)
+    print ("max_nodes_in_quene:",max_nodes_in_quene)
+    break_quene_times = 0
 
     #Outside loop continues until there are no nodes left
     while not nodeDoneTable.all():
 
         # --- update NewExtents for the data_delivery_worker for the newly added nodes ---
-        q_data_requests.put( [ "NewExtents", [ [ currentTopSlice_image1, currentBottomSlice_image1 ], [ currentTopSlice_image2, currentBottomSlice_image2 ] ] ] )
+        if break_quene_times == 0:
+            q_data_requests.put( [ "NewExtents", [ [ currentTopSlice_image1, currentBottomSlice_image1 ], [ currentTopSlice_image2, currentBottomSlice_image2 ] ] ] )
         # --------------------------------------------------
 
         # reset node counter -- in order to know when to stop...
@@ -177,6 +217,9 @@ def DIC_setup( kinematics, data, q_data_requests, workerQueues ):
                 nodeDoneTable[ nodeNumber ] = True
                 # Add one to node counter...
                 nodesToProcess += 1
+            if nodesToProcess >  max_nodes_in_quene:
+                break_quene_times = break_quene_times + 1
+                break
 
         # Checking if all nodes have been sent to the worker add STOP to queue to stop the DIC_workers
         if nodeDoneTable.all():
